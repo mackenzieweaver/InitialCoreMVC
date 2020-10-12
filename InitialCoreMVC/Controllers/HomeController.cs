63d8653aaea69f6dbebe3e85fde61beac4d55bd7
Mack <macknz7@gmail.com>
2020-10-12 14:39:54 -0700

    Initial commit

---
+++ InitialCoreMVC/Controllers/HomeController.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using InitialCoreMVC.Models;
+
+namespace InitialCoreMVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        public IActionResult Code()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Solution()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Solution(string userText)
+        {
+            if(string.IsNullOrWhiteSpace(userText))
+            {
+                return View();
+            }
+            var result = userText.ToLower().Replace(" ", "");
+            var reverse = string.Join("", result.Reverse().ToArray());
+            ViewData["Input"] = userText;
+            ViewData["Reversed"] = reverse;
+            ViewData["Result"] = userText == reverse;
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ InitialCoreMVC/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace InitialCoreMVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
